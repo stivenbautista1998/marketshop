36a0e04ebb7c0f5c766306c2c6a056741dcb48a0
Stiven Bautista <stivenb1994@gmail.com>
2021-12-25 21:54:28 -0500

    the home view has been developed and its styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 
 window.addEventListener("load", function() {
     console.log("working!!");
+    const btnLogin = this.document.getElementById("login-btn");
+
+    btnLogin.addEventListener("click", function() {
+        window.location.href = "/views/home.html";
+    });
 });
