85bbd20e057ef16811c6fc72f04c46c1fa0a9759
Stiven Bautista <stivenb1994@gmail.com>
2021-12-27 18:25:39 -0500

    creating scrollbar to the nav on home section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 
 window.addEventListener("load", function() {
     console.log("working!!");
-    const btnLogin = this.document.getElementById("login-btn");
+    /* console.log(window.location.href); */
+    if(window.location.href == "http://127.0.0.1:5500/index.html") {
+        const btnLogin = this.document.getElementById("login-btn");
+    
+        btnLogin.addEventListener("click", function() {
+            window.location.href = "/views/home.html";
+        });
+    }
 
-    btnLogin.addEventListener("click", function() {
-        window.location.href = "/views/home.html";
-    });
+    if(window.location.href == "http://127.0.0.1:5500/views/home.html") {
+        /* const homeNav = this.document.getElementById("home-nav"); */
+    }
 });
