f5973f4efa798a0164203f5cda50f045e4faa6b0
Stiven Bautista <stivenb1994@gmail.com>
2021-12-29 14:56:57 -0500

    functionality of adding the selected products to the shopping card tab on the home section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-var btnHomeMenu = null, userData = null, btnShowShoppingCard = null, linkLogo, textLogo;
-var users = fetch("../data/users.json")
+var btnHomeMenu = null, userData = null, arrayProducts = [], btnShowShoppingCard = null, linkLogo, textLogo, 
+wrapperHomeShoppItems;
+
+fetch("../data/users.json")
 .then(data => data.json())
 .then(result => {
     userData = result.login[0];
 });
+
+fetch("../data/products.json")
+.then(data => data.json())
+.then(result => {
+    arrayProducts = result.product;
+    console.log(arrayProducts);
+}).catch(err => console.log(err));
 
 window.addEventListener("load", function() {
     /* console.log(window.location.href); */
@@ -21,6 +30,7 @@ window.addEventListener("load", function() {
         btnShowShoppingCard = this.document.getElementById("js-shopping-card-tab");
         linkLogo = this.document.getElementById("js-link-logo");
         textLogo = this.document.getElementById("js-tittle-logo");
+        wrapperHomeShoppItems =  this.document.getElementById("js-shopping-container-items");
         
         let lastScrollY = window.scrollY;
 
@@ -89,5 +99,43 @@ function showShoppingCard() {
         document.body.style.overflow = "hidden";
         linkLogo.classList.add("hide-logo");
         textLogo.classList.remove("hide-logo");
+        showProductsSelected();
     }
 }
+
+function showProductsSelected() {
+    let concatArticles = "";
+    let shoppingCardItems = userData.currentSelectedProducts.map(item => {
+        var infoProduct = null;
+        for(let obj in arrayProducts) {
+            if(arrayProducts[obj].id == item.idProduct) {
+                infoProduct = arrayProducts[obj];
+                console.log(infoProduct);
+                concatArticles += "<article class='shopping-card-item'>" +
+                                        "<div class='front-container'>" +
+                                            "<div class='image-border'>" +
+                                                "<img class='image-shopping-card' src='" + infoProduct.imgs[0].img + "' alt='image of product'>" +
+                                            "</div>" +
+                                            "<span class='name-product'>" + infoProduct.name + "</span>" +
+                                        "</div>" +
+                                        "<div class='back-container'>" +
+                                            "<span class='price-product'>$ " + infoProduct.price + ",00</span>" +
+                                            "<img onclick='deleteItem(this)' data-product='" + infoProduct.id + "' class='close-icon close-item' src='../assets/icons/x-icon.svg' alt='close item'>" +
+                                        "</div>" +
+                                    "</article>";
+                break;
+            }
+        }
+        return concatArticles;
+    });
+    wrapperHomeShoppItems.innerHTML = concatArticles;
+}
+
+function deleteItem(idElement) {
+    console.log("It works");
+}
+
+
+
+
+
